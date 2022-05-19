bc2a7d3397b35187adcac72954b7c0edfd6fb6f5
rddomino <seltos@bk.ru>
2022-05-19 14:07:44 +0600

    Создание главной страницы с модальным окном-формой для создания заявки

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,34 @@
 <template>
-  <div class="home">
-  </div>
+  <app-page title="Список заявок">
+    <template #header>
+      <button class="btn primary" @click="modal = true">Создать</button>
+    </template>
+
+    <request-table :requests="[]"></request-table>
+
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false">
+        <request-modal @created="modal = false"/>
+      </app-modal>
+    </teleport>
+  </app-page>
 </template>
 
 <script>
+import {ref} from "vue";
+import AppPage from "../components/ui/AppPage";
+import RequestTable from "../components/request/RequestTable";
+import AppModal from "../components/ui/AppModal";
+import RequestModal from "../components/request/RequestModal";
+
 export default {
-  name: 'Home',
+  setup() {
+    const modal = ref(false)
+
+    return {
+      modal,
+    }
+  },
+  components: {AppPage, RequestTable, AppModal, RequestModal}
 }
 </script>
